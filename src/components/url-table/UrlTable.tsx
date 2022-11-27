cc29dafb8fc756c4cebe77d39f54e65152f1aeaf
fkaspare <fabian.kasparek@stud.htwk-leipzig.de>
2022-11-27 09:45:13 +0100

    Refactoring and add icon

--- src/components/url-table/UrlTable.tsx
+++ src/components/url-table/UrlTable.tsx
@@ -1,26 +1,13 @@
 import React from "react";
-import { styled } from '@mui/material/styles';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
 import DeleteForever from "@mui/icons-material/DeleteForever";
 import { UrlTableProps } from "./types";
  
 export const UrlTable = ({ urls, deleteUrl }: UrlTableProps) => {
-  const StyledPaper = styled(Paper)(({ theme }: { theme: any }) => ({
-    border: "2px solid",
-    borderColor: theme.palette.primary.main
-  }));
-
-  const StyledTableHeadColumn = styled(TableCell)(({ theme }: { theme: any }) => ({
-    borderBottom: urls.length == 0 ? "0px" : "2px solid",
-    borderColor: theme.palette.primary.main
-  }));
-
   const openUrl = (url: string) => {
     try {
       window.open(url);
@@ -29,27 +16,19 @@ export const UrlTable = ({ urls, deleteUrl }: UrlTableProps) => {
   };
 
   return (
-    <StyledPaper sx={{ width: 600, mt: "1%" }}>
-      <TableContainer sx={{ maxHeight: 300 }}>
-        <Table sx={{ border: "2px solid primary" }} stickyHeader size="small">
-          <TableHead>
-            <TableRow>
-              <StyledTableHeadColumn sx={{ fontSize: "90%", fontWeight: "bold" }} align="left">URL</StyledTableHeadColumn>
-              <StyledTableHeadColumn align="right"></StyledTableHeadColumn>
+    <TableContainer sx={{ maxHeight: 300, width: 600, mt: "1%" }}>
+      <Table sx={{ border: "2px solid primary" }} stickyHeader size="small">
+        <TableBody>
+          {urls.map((url, index) => (
+            <TableRow key={index}>
+              <TableCell align="left" onClick={() => openUrl(url.url)}>{url.url}</TableCell>
+              <TableCell align="right">
+                <DeleteForever sx={{ mt: 0.5 }} color="primary" onClick={() => deleteUrl(url.id)} />
+              </TableCell>
             </TableRow>
-          </TableHead>
-          <TableBody>
-            {urls.map((url, index) => (
-              <TableRow key={index}>
-                <TableCell align="left" onClick={() => openUrl(url.url)}>{url.url}</TableCell>
-                <TableCell align="right">
-                  <DeleteForever sx={{ mt: 0.5 }} color="primary" onClick={() => deleteUrl(url.id)} />
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </StyledPaper>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
